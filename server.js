ae7a2573ddb10eeefb73080d968046369e43db39
Divyang KOladara <divukoladara@gmail.com>
2022-02-01 18:14:56 +0530

    chane in server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URL,
     { 
