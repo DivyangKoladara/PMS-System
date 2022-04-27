2597d655c8cc01772dbfead8385fd7ad7cead959
DivyangKoladara <divukoladara@gmail.com>
2022-04-27 16:33:16 +0530

    test

--- controller/createproject/index.js
+++ controller/createproject/index.js
@@ -7,20 +7,21 @@ const imageindex = require('../../services/imageupload/index')
 
 const upload = require('../../services/imageupload/multer')
 
+const authontication = require('../users/Users.controller')
 
 
 
-router.route('/createproject').post(upload.single('image'),user.createProject);
+router.route('/createproject').post(upload.single('image'),authontication.tokenVerify,user.createProject);
 
-router.route('/deleteproject').post(user.deleteProject);
+router.route('/deleteproject').post(authontication.tokenVerify,user.deleteProject);
 
-router.route('/editproject').put(upload.single('image'),user.editproject);
+router.route('/editproject').put(upload.single('image'),authontication.tokenVerify,user.editproject);
 
-router.route('/assignproject').post(user.assignProject);
+router.route('/assignproject').post(authontication.tokenVerify,user.assignProject);
 
-router.route('/projectassigndetails').get(user.projectAssignDetails);
+router.route('/projectassigndetails').get(authontication.tokenVerify,user.projectAssignDetails);
 
-router.route('/projectlist').get(user.projectList)
+router.route('/projectlist').get(authontication.tokenVerify,user.projectList)
 
 
 
